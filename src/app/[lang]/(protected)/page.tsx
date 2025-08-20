1207af3e18a6abb5e955c56b0782bd53d93fcc49
Bomberkill <ronaldkamgaing4@gmail.com>
2025-08-21 00:22:04 +0100

    fix: build error

--- src/app/[lang]/(protected)/page.tsx
+++ src/app/[lang]/(protected)/page.tsx
@@ -1,7 +1,7 @@
 // "use client"
 import { getDictionary } from "@/app/getDictionary";
 import WorkInProgress from "@/components/WorkInProgress";
-import Image from "next/image";
+// import Image from "next/image";
 // import file from "@/public/file.svg";
 // import window from "../../../public/file.svg";
 // import globe from "@/public/globe.svg";
@@ -12,7 +12,10 @@ export default async function Home({params}: {params: Promise<{lang: 'en' | 'fr'
   const {lang} = await params
   const dict = await getDictionary(lang)
   return (
+    <>
+    <span className="hidden">{dict?.home.title}</span>
     <WorkInProgress/>
+    </>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
     //     <Image
