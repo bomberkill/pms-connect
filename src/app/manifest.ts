5cfe7d4ea4d5e29f5230f7934ea62cb0bd7a69bb
Bomberkill <ronaldkamgaing4@gmail.com>
2025-09-05 13:32:43 +0100

    fix: change PWA configuration

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,28 +5,24 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Pms-Connect",
     short_name: "Pms-Connect",
     description: "The professional social network for healthcare providers.",
-    start_url: "/",
+    start_url: "/login",
     display: "standalone",
+    orientation: "portrait",
     background_color: "#ffffff",
     theme_color: "#000000",
     icons: [
         {
-            src: "/icons/icon-192x192.png",
+            src: "/web-app-manifest-192x192.png",
             sizes: "192x192",
-            type: "image/png"
-        },
-        {
-            src: "/icons/icon-512x512.png",
-            sizes: "512x512",
             type: "image/png",
-            purpose: "any"
+            purpose: "maskable"
         },
         {
-            src: "/icons/icon-maskable-512x512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable"
-        }
+        },
     ]
   }
 }
